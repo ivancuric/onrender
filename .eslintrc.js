7fd67f1b6421e7b394d914e26207097363f53e89
Ivan Čurić <ivan.curic@gmail.com>
2016-06-11 23:53:48 +0200

    added gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "jquery": true,
+  },
+  "extends": ["eslint:recommended"],
+  "plugins": [],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "globals": {
+    "log": true,
+    "raf": true,
+    "sto": true,
+    "moment": true,
+    "Pikaday": true
+  },
+  "rules": {
+    "no-console": 0,
+    "no-unused-vars": [2, { "args": "none" }],
+    'no-unused-vars': 0,
+    "no-unused-expressions": 2,
+    "quotes": [
+      2,
+      "single"
+    ],
+    "semi": [
+      2,
+      "always"
+    ],
+  },
+};
