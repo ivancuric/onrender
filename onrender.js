565d7330f638bba8c77b6882e83eeaf4f6ed5439
Ivan Čurić <ivan.curic@gmail.com>
2016-06-15 13:16:30 +0200

    No ES6

--- onrender.js
+++ onrender.js
@@ -1,25 +1,24 @@
 'use strict';
 
-(function() {
-  // Check if used as a standalone script or a node module
-  if (typeof exports == 'object') {
-    module.exports = onRender;
-  } else {
-    window.onRender = onRender;
-  }
+// Takes a callback which is fired when the element is rendered
+function onRender(element, callback) {
 
-  // Takes a callback which is fired when the element is rendered
-  function onRender(element, callback) {
-    // Element can be jQuery or native
-    if (element instanceof jQuery) {
-      element = element[0];
-    }
-    // Pop a forced layout tied to a promise
-    Promise.resolve(element.getBoundingClientRect())
-    .then(function(value) {
-      requestAnimationFrame(function() {
-        callback(value);
-      });
+  // Element can be jQuery or vanilla JS
+  if (element instanceof jQuery) element = element[0];
+
+  // Force a layout which returns a promise
+  Promise.resolve(element.getBoundingClientRect())
+  .then(function() {
+    requestAnimationFrame(function() {
+      callback();
     });
-  }
-})();
+  });
+  console.log('in module');
+}
+
+// Check if used as a standalone script or a node module
+if (typeof exports === 'object') {
+  module.exports = onRender;
+} else {
+  window.onRender = onRender;
+}
