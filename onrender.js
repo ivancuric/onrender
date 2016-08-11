5d81e4a9ad244885fa23dced0edc304345148ded
Ivan Čurić <ivan.curic@gmail.com>
2016-08-11 14:23:16 +0200

    Comments

--- onrender.js
+++ onrender.js
@@ -3,10 +3,10 @@
 // Takes a callback which is fired when the element is rendered
 function onRender(element, callback) {
 
-  // Force a layout which returns a promise
+  // Force a layout which returns a Promise
   Promise.resolve(element.getBoundingClientRect())
   .then(function() {
-    // needs 2 frames to optimize properly
+    // needs 2 frames for `will-change` to get to render (src: Paul Lewis)
     requestAnimationFrame(function() {
       requestAnimationFrame(function() {
         callback();
